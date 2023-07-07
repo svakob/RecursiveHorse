40ce027e908c0e7b0a94b164653c0d892edd2942
Sviatoslav <svakob1710@gmail.com>
2023-07-07 18:08:12 +0300

    developing init

--- RecursiveHorse/RecursiveHorse.cpp
+++ RecursiveHorse/RecursiveHorse.cpp
@@ -1,6 +1,50 @@
 #include "pch.h"
 
+class cell {
+    
+public:
+	std::vector<cell*> next_cells;
+	cell()
+	{
+		
+	}
+};
+
+void print(std::vector<std::vector<cell>> bord) {
+	for (auto& i : bord)
+	{
+		for (auto& j : i)
+		{
+			std::cout << " x";
+		}
+		std::cout << '\n';
+	}
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector<std::vector<cell>> bord{8, std::vector<cell>{8}};
+
+	for (int i = 0; i < bord.size(); i++)
+	{
+		for (int j = 0; j < bord[i].size(); j++)
+		{
+			if (i == 0 && j == 0) {
+				bord[0][0].next_cells.push_back(&bord[1][2]);
+				bord[0][0].next_cells.push_back(&bord[2][1]);
+			}
+			else if (i == 0 && j == bord[i].size()-1) {
+				bord[0][j].next_cells.push_back(&bord[1][bord[0].size() - 3]);
+				bord[0][j].next_cells.push_back(&bord[2][bord[0].size() - 2]);
+			}
+			else if (i == bord.size() - 1 && j == 0) {
+				bord[i][0].next_cells.push_back(&bord[bord.size() - 3][1]);
+				bord[i][0].next_cells.push_back(&bord[bord.size() - 2][2]);
+			}
+			else if (i == bord.size() - 1 && j == bord[i].size() - 1) {
+				bord[i][j].next_cells.push_back(&bord[bord.size() - 3][bord[0].size() - 2]);
+				bord[i][j].next_cells.push_back(&bord[bord.size() - 2][bord[0].size() - 3]);
+			}
+		}
+	}
 }
